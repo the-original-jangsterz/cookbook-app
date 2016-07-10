e55ad76edb63f6b31e528426385e7e905598d522
Peter Jang <peter.x.jang@gmail.com>
2016-07-10 16:16:56 -0500

    Add restful show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/recipes' => 'recipes#index'
+  get '/recipes/:id' => 'recipes#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
