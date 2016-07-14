ac1e6c6f0a2d9b1a5218a445607693063cbbe757
Peter Jang <peter.x.jang@gmail.com>
2016-07-13 19:38:40 -0500

    Show ingredients as unordered list using model method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ActiveRecord::Base
+  def ingredients_list
+    list = []
+    ingredients.split(', ').each do |ingredient|
+      list << ingredient.upcase
+    end
+    list
+  end
 end
