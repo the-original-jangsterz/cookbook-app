1becb9aa7966ed945aa93c526a0af61373050150
Peter Jang <peter.x.jang@gmail.com>
2016-07-13 20:01:15 -0500

    Add friendly created at and prep time model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,17 @@ class Recipe < ActiveRecord::Base
     end
     list
   end
+
+  def friendly_created_at
+    created_at.strftime('%A, %d %b %Y %l:%M %p')
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
